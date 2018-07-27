08a1c5976f5e0861d06d33704684b313dc6d168b
Willy Nzesseu <willynzesseu@gmail.com>
2018-07-27 03:08:25 +0100

    ok

--- App/Controllers/Home.php
+++ App/Controllers/Home.php
@@ -3,7 +3,7 @@
 namespace App\Controllers;
 
 use \Core\View;
-
+use \App\Models\User;
 /**
  * Home controller
  *
@@ -20,5 +20,19 @@ class Home extends \Core\Controller
     public function indexAction()
     {
         View::renderTemplate('Home/index.html');
+    }
+
+    /**
+     * Show the login page
+     *
+     * @return void
+     */
+    public function loginAction()
+    {
+        return 5;
+        $user =new User();
+        $user->email= $_POST["email"];
+        $user->password=$_POST["password"];
+        View::renderTemplate('Home/login.html');
     }
 }

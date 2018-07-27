321811bc03a3b8a94e22f9d27b71368c8d1116a5
Willy Nzesseu <willynzesseu@gmail.com>
2018-07-27 03:07:06 +0100

    res

--- public/index.php
+++ public/index.php
@@ -26,6 +26,7 @@ $router = new Core\Router();
 
 // Add the routes
 $router->add('', ['controller' => 'Home', 'action' => 'index']);
+$router->add('login', ['controller' => 'Home', 'action' => 'login']);
 $router->add('{controller}/{action}');
     
 $router->dispatch($_SERVER['QUERY_STRING']);
